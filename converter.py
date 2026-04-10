3748235707d47cc3f0db5363905401ea63529feb
Goldenn Vscodov <Sraniparol1@gmail.com>
2026-04-11 03:32:45 +0500

    Добавлена валидация ввода №3

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 import json
 
 def convert_currency(amount, currency):
-    # Задача №1: Добавили GBP (фунт)
     rates = {"USD": 92.5, "EUR": 100.2, "GBP": 115.7}
     
     if currency not in rates:
@@ -15,14 +14,23 @@ def convert_currency(amount, currency):
     }
 
 def main():
-    amt = float(input("Введите сумму в рублях: "))
+    print("=== МИКРОСЕРВИС КОНВЕРТАЦИИ ===")
+    
+    # Задача №3: Валидация (чтобы не падало от букв)
+    try:
+        amt = float(input("Введите сумму в рублях: "))
+    except ValueError:
+        print("="*30)
+        print("ОШИБКА: Введите числовое значение!")
+        print("="*30)
+        return
+
     curr = input("Введите целевую валюту (USD, EUR, GBP): ").upper()
     
     result = convert_currency(amt, curr)
     
-    # Задача №2: Красивый вывод с разделителями
     print("="*30)
-    print("РЕЗУЛЬТАТ КОНВЕРТАЦИИ:")
+    print("РЕЗУЛЬТАТ ОПЕРАЦИИ:")
     print(json.dumps(result, indent=4, ensure_ascii=False))
     print("="*30)
 
